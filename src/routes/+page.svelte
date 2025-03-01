803ddbf132559cb286c01f0b26fe15aa8e7106a9
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-03-01 07:45:48 +0700

    Enhance filtering functionality with multi-category selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,17 +12,17 @@
 	import { goto } from '$app/navigation';
 
 	// filters state
-	let category = $state('');
+	let category: Category[] = $state([]);
 	let language = $state('');
 	let keyword = $state(page.url.searchParams.get('search')?.toLowerCase() ?? '');
 
 	const filterResource = (
-		c: Category | string = '',
+		c: Category[] | null,
 		l: Language | string = '',
 		q: string = ''
 	): Resource[] => {
 		return resources.filter(({ categories, label, languages, url }) => {
-			const matchesCategory = c ? categories.includes(c as Category) : true;
+			const matchesCategory = c ? c.every((cat) => categories.includes(cat)) : true;
 			const matchesLanguage = l
 				? (languages?.some((lang) => lang.toLowerCase() === l.toLowerCase()) ?? false)
 				: true;
@@ -46,6 +46,15 @@
 
 		goto(`?${params.toString()}`, { replaceState: true });
 	};
+
+	const filterCategory = (c: Category) => {
+		if (category.includes(c)) {
+			category = category.filter((cat) => cat != c);
+			return;
+		}
+		category.push(c);
+		console.log(category);
+	};
 </script>
 
 <svelte:head>
@@ -58,60 +67,82 @@
 </svelte:head>
 
 <main class="mx-auto my-10 max-w-screen-lg px-3">
-	<div
-		class="sticky top-0 z-50 my-6 flex scroll-m-4 flex-wrap items-center justify-between bg-gray-100 px-2 py-4"
-	>
-		<div class="">
-			<h1 class="text-3xl font-semibold">Dev bookmarks</h1>
-			<p>A list of useful websites for developers</p>
-			<a class="text-xs underline" href="https://github.com/rifkiahmadfahrezi/dev-bookmarks/" target="_blank"
-				>contribute</a
+	<div class="sticky top-0 z-50 my-6 bg-gray-100 px-2 pt-2 pb-4">
+		<div class="flex scroll-m-4 flex-wrap items-center justify-between">
+			<div class="">
+				<h1 class="text-3xl font-semibold">Dev bookmarks</h1>
+				<p>A list of useful websites for developers</p>
+				<a
+					class="text-xs underline"
+					href="https://github.com/rifkiahmadfahrezi/dev-bookmarks/"
+					target="_blank">contribute</a
+				>
+			</div>
+			<div
+				class="mt-3 flex grow items-center justify-between rounded-md border border-gray-300 bg-white pr-3 md:mt-0 md:grow-0"
 			>
+				<input
+					bind:value={keyword}
+					onchange={handleSearch}
+					class="w-full max-w-sm px-4 py-2"
+					type="search"
+					placeholder="Search..."
+				/>
+				<select
+					title="select language"
+					aria-label="select language"
+					bind:value={language}
+					class="px-4 py-2"
+					name="select-language"
+					id="select-language"
+				>
+					<option selected value="" disabled>Language</option>
+					<option value="">All</option>
+					{#each languages as language}
+						<option value={language}>{language}</option>
+					{/each}
+				</select>
+			</div>
 		</div>
-		<div
-			class="mt-3 flex grow items-center justify-between rounded-md border border-gray-300 bg-white pr-3 md:mt-0 md:grow-0"
-		>
-			<input
-				bind:value={keyword}
-				onchange={handleSearch}
-				class="w-full max-w-sm px-4 py-2"
-				type="search"
-				placeholder="Search..."
-			/>
-			<select
-				title="select category"
-				aria-label="select category"
-				bind:value={category}
-				class="px-4 py-2"
-				name="category"
-				id="category"
-			>
-				<option selected value="" disabled>Category</option>
-				<option value="">All</option>
-				{#each categories as category}
-					<option value={category}>{category}</option>
+		<details class="my-4 w-full">
+			<summary class="hidden w-fit rounded-full bg-blue-500 px-2 py-1 text-blue-50 md:block">
+				<span id="filter-toggle" role="term" aria-details="toggle-category-filter" class="hidden"
+					>Filter by category</span
+				>
+			</summary>
+		</details>
+		<div role="definition" id="toggle-category-filter" class="content">
+			<div class="my-2 flex flex-wrap gap-1">
+				{#each categories as c}
+					<label class="rounded-full bg-gray-300 px-2 py-1" for={c}>
+						<input
+							checked={category.includes(c)}
+							onclick={() => filterCategory(c)}
+							type="checkbox"
+							name={c}
+							id={c}
+						/>
+						{c}
+					</label>
 				{/each}
-			</select>
-			<select
-				title="select language"
-				aria-label="select language"
-				bind:value={language}
-				class="px-4 py-2"
-				name="language"
-				id="language"
-			>
-				<option selected value="" disabled>Language</option>
-				<option value="">All</option>
-				{#each languages as language}
-					<option value={language}>{language}</option>
-				{/each}
-			</select>
+			</div>
 		</div>
+		{#if category.length != 0}
+			<div class="flex flex-wrap items-center gap-1">
+				<span>Categories</span>
+				{#each category as c}
+					<span class="rounded-full bg-blue-200 px-2 py-1 text-blue-950">{c}</span>
+				{/each}
+			</div>
+			<button class="rounded-md bg-red-500 px-2 py-1 text-white" onclick={() => (category = [])}
+				>clear</button
+			>
+		{/if}
 	</div>
 	{#if filteredResource.length === 0}
 		<p>{keyword ? `No results on '${keyword}'` : 'There is no resources to show'}</p>
 	{:else}
-		<div class="my-10 flex flex-wrap gap-4">
+		<div class="my-5 flex flex-wrap gap-4">
 			{#each filteredResource as resource}
 				<a
 					title={resource.label}
@@ -157,3 +188,59 @@
 		</div>
 	{/if}
 </main>
+
+<style>
+	details {
+		overflow: hidden; /* Keep this line to prevent an odd blue outline around the element in Safari. */
+	}
+
+	summary {
+		display: block;
+	}
+
+	summary::-webkit-details-marker {
+		display: none;
+	}
+
+	#filter-toggle {
+		position: relative;
+		display: flex;
+		align-items: center;
+	}
+
+	#filter-toggle:hover {
+		cursor: pointer;
+	}
+
+	#filter-toggle::before {
+		content: '+';
+		font-size: 1rem;
+		display: flex;
+		align-items: center;
+		margin-right: 0.5rem;
+		transition: rotate 200ms 400ms ease-out;
+	}
+
+	div.content {
+		box-sizing: border-box;
+		max-height: 0;
+		overflow: hidden;
+		padding: 0 10px;
+		border: 2px solid transparent;
+		transition:
+			max-height 400ms ease-out,
+			border 0ms 400ms linear;
+	}
+
+	details[open] + div.content {
+		max-height: 100vh;
+		transition:
+			max-height 400ms ease-out,
+			border 0ms linear;
+	}
+
+	details[open] #filter-toggle::before {
+		rotate: 90deg;
+		transition: rotate 200ms ease-out;
+	}
+</style>
