bf20208443c53ee0e8207315529501963f55869b
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-05-04 11:58:42 +0700

    refactor TopHeader component for improved styling and structure

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -1,14 +1,23 @@
 <template>
-   <header class="p-3 sticky top-0 flex items-center justify-between">
-      <UButton color="info" href="/" icon="i-lucide-bookmark">
-         <span class="sr-only">Dev bookmarks</span>
-      </UButton>
+   <header class="sticky top-0 flex items-center justify-between z-50">
+      <div class="p-3 bg-neutral-900 rounded-br-2xl relative">
+         <UButton 
+            color="info" href="/" icon="i-lucide-bookmark">
+            <span class="sr-only">Dev bookmarks</span>
+         </UButton>
+      </div>
 
-      <div class="flex items-center gap-2">
-         <UButton icon="i-lucide-github" color="secondary" variant="ghost">
+      <div class="flex items-center gap-2 p-3 bg-neutral-900 rounded-bl-2xl">
+         <UButton 
+            href="https://github.com/rifkiahmadfahrezi/dev-bookmarks/"
+            target="_blank"
+            con="i-lucide-github" color="secondary" variant="ghost">
             <span class="sr-only">Github</span>
          </UButton>
-         <UButton color="secondary">
+         <UButton
+            href="https://github.com/rifkiahmadfahrezi/dev-bookmarks/"
+            target="_blank"
+            color="secondary">
             Submit
          </UButton>
       </div>
