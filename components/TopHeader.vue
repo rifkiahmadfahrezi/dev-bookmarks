9a12ab53319a8bf2237c667ec466a5faa0433d49
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-05-05 20:09:04 +0700

    fix: update TopHeader styles and replace Submit button with CreateBookmark component

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -1,7 +1,7 @@
 <template>
    <header class="sticky top-0 flex items-center justify-between max-w-screen-xl mx-auto isolate">
       <span aria-hidden="true" class="fixed left-0 top-0 h-5 w-full z-10 bg-neutral-900"></span>
-      <div class="p-3 bg-neutral-900 rounded-br-2xl relative z-50">
+      <div class="p-3 bg-default mt-5 rounded-br-2xl relative z-50">
          <NuxtLink 
             href="/">
             <NuxtImg src="/logo.svg" class="size-8" preload />
@@ -9,19 +9,14 @@
          </NuxtLink>
       </div>
 
-      <div class="flex items-center gap-2 p-3 bg-neutral-900 rounded-bl-2xl z-50">
+      <div class="flex items-center gap-2 p-3 bg-default mt-5 rounded-bl-2xl z-50">
          <UButton 
             href="https://github.com/rifkiahmadfahrezi/dev-bookmarks/"
             target="_blank"
             icon="i-lucide-github" color="secondary" variant="ghost">
             <span class="sr-only">Github</span>
          </UButton>
-         <UButton
-            href="https://github.com/rifkiahmadfahrezi/dev-bookmarks/"
-            target="_blank"
-            color="secondary">
-            Submit
-         </UButton>
+         <CreateBookmark />
       </div>
    </header>
 </template> 
